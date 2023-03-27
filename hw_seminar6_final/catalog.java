4212fbb71a6daf59db0d9d5eeb8b9716240f6a1c
Yana Priyma <yanaprima@rambler.ru>
2023-03-28 00:25:25 +0300

    финальное ДЗ - семинар 6

---
+++ hw_seminar6_final/catalog.java
@@ -0,0 +1,7 @@
+package hw_seminar6_final;
+
+public class catalog extends laptop {
+    public catalog(int id, int ram, int ssd, String os, String color){
+        super(id,ram,ssd,os,color);
+    }
+}
---
+++ hw_seminar6_final/laptop.java
@@ -0,0 +1,23 @@
+package hw_seminar6_final;
+
+public class laptop {
+    int id;
+    int ram;
+    int ssd;
+    String os;
+    String color;
+    public laptop(int id, int ram, int ssd, String os, String color){
+        this.id = id;
+        this.ram = ram;
+        this.ssd = ssd;
+        this.os = os;
+        this.color = color;
+    }
+    public String toString() {
+        return String.format("id: %d, ram: %d ГБ, ssd: %d ГБ, OS: %s, color: %s", id, ram, ssd, os, color);
+    }
+    public boolean equals(Object o) {
+        laptop t = (laptop) o;
+        return id == t.id;
+    }
+}
